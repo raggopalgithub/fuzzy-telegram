503b26c55fd45948a884f0b954d19affbf4a5a20
EC2 Default User <ec2-user@ip-172-31-41-66.us-east-2.compute.internal>
2020-07-17 23:53:24 +0000

    inital push

---
+++ phinx.php
@@ -0,0 +1,18 @@
+<?php
+    return array(
+        "paths" => array(
+            "migrations" => "migrations"
+        ),
+        "environments" => array(
+            "default_migration_table" => "phinxlog",
+            "default_database" => "default",
+            "default" => array(
+                "adapter" => "mysql",
+                //"adapter" => "sqlite",
+                "host" => getenv('RDS_HOSTNAME'),
+                "name" => getenv('RDS_DB_NAME'),
+                "user" => getenv('RDS_USERNAME'),
+                "pass" => getenv('RDS_PASSWORD')
+            )
+        )
+    );
